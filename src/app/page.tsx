292a6b9781288b3af594e1b232f89fc3ad299938
John Royal <34844819+john-royal@users.noreply.github.com>
2024-09-10 22:19:55 -0400

    feat: install shadcn and add styling for login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,24 @@
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { lucia, validateRequest } from "~/server/auth";
+import { signOut } from "~/server/actions";
+import { validateRequest } from "~/server/auth";
 import { Editor } from "./_components/editor";
 
 export default async function Page() {
   const { user } = await validateRequest();
+
   if (!user) {
     return redirect("/login");
   }
+
   return (
     <>
       <h1 className="text-2xl font-bold">Hi, {user.username}!</h1>
       <p>Your user ID is {user.id}.</p>
       <p>Your GitHub ID is {user.githubId}.</p>
       <Editor />
-      <form action={logout}>
+      <form action={signOut}>
         <button>Sign out</button>
       </form>
     </>
   );
-}
-
-async function logout() {
-  "use server";
-
-  const { session } = await validateRequest();
-  if (!session) {
-    return {
-      error: "Unauthorized",
-    };
-  }
-
-  await lucia.invalidateSession(session.id);
-
-  const sessionCookie = lucia.createBlankSessionCookie();
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes,
-  );
-
-  redirect("/login");
 }
---
+++ src/server/actions.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { generateState } from "arctic";
+import { github, lucia, validateRequest } from "./auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signInWithGitHub() {
+  const state = generateState();
+  const url = await github.createAuthorizationURL(state);
+
+  cookies().set("github_oauth_state", state, {
+    path: "/",
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 60 * 10,
+    sameSite: "lax",
+  });
+
+  redirect(url.toString());
+}
+
+export async function signOut() {
+  const { session } = await validateRequest();
+  if (!session) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+
+  redirect("/login");
+}
