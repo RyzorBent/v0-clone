ec4c81f1d42a491e4f99f82e5597c83010d440f7
John Royal <34844819+john-royal@users.noreply.github.com>
2024-09-20 22:04:24 -0400

    chore(web): delete remix frontend

--- packages/web-remix/app/lib/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-
-import type { Dispatch, State } from "./store";
-
-export const useTypedSelector = useSelector.withTypes<State>();
-export const useTypedDispatch = useDispatch.withTypes<Dispatch>();
