e6cb05ac6aea0fa5fd303e64226897271e1c44a8
John Royal <34844819+john-royal@users.noreply.github.com>
2025-02-03 23:50:42 -0500

    refactor: remove unused secret

--- infra/secrets.ts
+++ infra/secrets.ts
@@ -1,7 +1,6 @@
 export const secrets = {
   ClerkIssuer: new sst.Secret("ClerkIssuer"),
   ClerkPublishableKey: new sst.Secret("ClerkPublishableKey"),
-  ClerkSecretKey: new sst.Secret("ClerkSecretKey"),
   DatabaseURL: new sst.Secret("DatabaseURL"),
   OpenAIAPIKey: new sst.Secret("OpenAIAPIKey"),
   PineconeAPIKey: new sst.Secret("PineconeAPIKey"),
